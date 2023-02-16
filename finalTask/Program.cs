b8b7fdd8c428f98a3c1070f889083b1d3f3e322a
Yauheni-Kauz-Siomin <Midnighter21@yandex.ru>
2023-02-16 10:30:54 +0300

    add method SortWord

---
+++ finalTask/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] array = new string[6] { "Hel", "lo", "World!", ":)", "1121212", "1124" };
+int len = 3;
+string[] sortArray;
+Console.Clear();
+PrintArray(SortWords(array, len));
+
+/* Простой вывоод на печать при помощи String.Join
+Console.WriteLine($"[{String.Join(",", SortWords(array, len))}]"); */
+
+/* Метод сортировки введеного пользователем массива и формирование нового массива, согласно
+заданным условиям */
+string[] SortWords(string[] array, int len)
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= len)
+        {
+            temp++;
+        }
+    }
+
+    string[] sortArray = new string[temp];
+    temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= len)
+        {
+            sortArray[temp++] = array[i];
+        }
+    }
+    return sortArray;
+}
